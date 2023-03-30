f98964805afe270a430fb0d06c51fd271cdc1151
abid-azad-1 <abidazad143@gmail.com>
2023-03-30 22:11:54 +0600

    added-cart and tax total price

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
@@ -15,13 +16,13 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // console.log(product)
-        const newCart=[...cart, product];
+        const newCart = [...cart, product];
         setCart(newCart);
     }
 
 
     return (
-        
+
         <div className='shop-container'>
             <div className='products-container'>
                 {
@@ -29,12 +30,11 @@ const Shop = () => {
                         key={product.id}
                         product={product}
                         handleAddToCart={handleAddToCart}
-                        ></Product>)
+                    ></Product>)
                 }
             </div>
             <div className='cart-container'>
-                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
 
         </div>
